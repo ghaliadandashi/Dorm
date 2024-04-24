5e20937174d127fd20722013d4cc02e41d1fc0d0
RahimjonB <rahimjon.berdiev@mail.ru>
2024-04-24 22:08:45 +0200

    updated loginForm

--- frontend/src/layout/Navbar.js
+++ frontend/src/layout/Navbar.js
@@ -1,28 +1,31 @@
-import React, {useEffect, useState} from "react";
-import {Link, useLocation, useNavigate} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
-import { faSearch , faCog, faComments, faBell} from '@fortawesome/free-solid-svg-icons';
-import {getAuth} from "firebase/auth";
-import axios, {get} from "axios";
-import {useAuth} from "../components/Auth/AuthHook";
-import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
-import '../styling/navbar.css'
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import axios, { get } from "axios";
+import { useAuth } from "../components/Auth/AuthHook";
+import { LogoutOutlined, UserOutlined } from "@ant-design/icons";
+import { Avatar } from "antd";
+import LoginForm from "../components/Auth/LoginForm";
+
+
+
 
 const Navbar = () => {
     const { isLoggedIn, role, status } = useAuth();
-    const icon = <FontAwesomeIcon icon={faUser} />;
     const currentPage = useLocation().pathname;
     const navigate = useNavigate();
     axios.defaults.withCredentials = true;
+    const [showLoginForm, setShowLoginForm] = useState(false);
 
     const handleLogout = () => {
         const auth = getAuth()
         const user = auth.currentUser
-        if(user){
-            auth.signOut().then(()=>navigate('/home'))
+        if (user) {
+            auth.signOut().then(() => navigate('/home'))
         }
-        axios.post('http://localhost:3001/api/logout', {},{withCredentials: true})
+        axios.post('http://localhost:3001/api/logout', {}, { withCredentials: true })
             .then(() => {
                 localStorage.clear();
                 navigate('/home');
@@ -33,72 +36,75 @@ const Navbar = () => {
             });
     };
 
-    if(status === 'Invalid'|| status === 'Pending'){
-        setTimeout(()=>{
+    if (status === 'Invalid' || status === 'Pending') {
+        setTimeout(() => {
             handleLogout()
-        },1000)
+        }, 1000)
 
-        return <div style={{height:'100vh',width:'100vw',display:'flex',justifyContent:'center',alignItems:'center',fontWeight:'bold',fontSize:'30px',color:'red'}}>ACCOUNT INVALID</div>
+        return <div style={{ height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center', fontWeight: 'bold', fontSize: '30px', color: 'red' }}>ACCOUNT INVALID</div>
     }
 
-    return(
+    const handleSigninClick = () => {
+        setShowLoginForm(true);
+    };
+
+    return (
         <>
-        <nav className="navbar">
-            <Link to='/' className="navbar-brand"><h3>Dorms</h3></Link>
-            <div className='search'>
-                <input className="search-bar" type="search" placeholder="Explore" />
-                <FontAwesomeIcon icon={faSearch} className='nav-link'/>
-            </div>
-            <ul className="navbar-nav">
-                {(isLoggedIn || status==='Valid') ? (
-                    <>
-                    <li className="nav-item">
-                    <Link to='/profile' className="nav-link">{icon}</Link>
-                    </li>
-            {(role === 'admin')?(
-                    <li className="nav-item">
-                        <Link to='/loginreq' className="nav-link">Login Requests</Link>
-                    </li>
-                ):null}
-                <li className="nav-item">
-                    <Link to='/settings' className="nav-link"><FontAwesomeIcon icon={faCog}/></Link>
-                </li>
-                        <li className="nav-item">
-                            <Link to='/chat' className="nav-link"><FontAwesomeIcon icon={faComments}/></Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to='/notifications' className="nav-link"><FontAwesomeIcon icon={faBell}/></Link>
-                        </li>
-                <li className="nav-item">
-                    <button onClick={handleLogout} className="nav-link" style={{backgroundColor:'#2b2b43',border:'none'}}><FontAwesomeIcon icon={faSignInAlt}/></button>
-                </li>
-            </>
-            ):(
-        <>
-            {currentPage === '/register' ? (
-                <li className="nav-item">
-                    <Link to='/login' className="nav-link">Login</Link>
-                </li>
-            ) : currentPage === '/home' || currentPage === '/' ? (
-                <>
-                    <li className="nav-item">
-                        <Link to='/login' className="nav-link">Login</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to='/register' className="nav-link">Register As Dorm Owner</Link>
-                    </li>
-                </>
-            ) : (
-                <li className="nav-item">
-                    <Link to='/register' className="nav-link">Register As Dorm Owner</Link>
-                </li>
+            <nav style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", alignContent: "space-between", width: "100vw" }}>
+                <Link to='/' style={{ color: "white", textDecoration: 'none' }}><h1>Dormy</h1></Link>
+
+                <ul style={{ display: "flex", flexDirection: "row", listStyle: "none", justifyContent: "space-between", gap: '10px', textDecoration: 'none', margin: '25px 5px' }}>
+                    {(isLoggedIn || status === 'Valid') ? (
+                        <>
+                            <li>
+                                <Link to='/profile' style={{ color: "black", textDecoration: 'none' }}><Avatar size="small" icon={<UserOutlined />} /></Link>
+                            </li>
+                            {(role === 'admin') ? (
+                                <li>
+                                    <Link to='/loginreq' style={{ color: "black", textDecoration: 'none' }}>Login Requests</Link>
+                                </li>
+                            ) : (role !== 'dormOwner' && role !== null) ?
+                                <li>
+                                    <Link to='/bookings' style={{ color: "black", textDecoration: 'none' }}>Bookings</Link>
+                                </li> : null}
+                            <li>
+                                <Link to='/contact' style={{ color: "black", textDecoration: 'none' }}>Contact Us</Link>
+                            </li>
+                            <li>
+                                <button onClick={handleLogout}><LogoutOutlined /></button>
+                            </li>
+                        </>
+                    ) : (
+                        <>
+                            {currentPage === '/register' ? (
+                                <li>
+                                    <Link to='/login' style={{ color: "black", textDecoration: 'none' }}>Sign-in</Link>
+                                </li>) : currentPage === '/home' || currentPage === '/' ? (
+                                    <>
+                                        <li>
+                                            <Link to='/login' style={{ color: "white", textDecoration: 'none' }}>Sign-in</Link>
+                                        </li>
+                                        <li>
+                                            <Link to='/register' style={{ color: "white", textDecoration: 'none' }}>Register As Dorm Owner</Link>
+                                        </li>
+                                    </>) : (
+                                        <li>
+                                            <Link to='/register' style={{ color: "black", textDecoration: 'none' }}>Register As Dorm Owner</Link>
+                                        </li>
+                                    )}
+                        </>
+                    )}
+
+                </ul>
+            </nav>
+
+            {showLoginForm && (
+                <div className="popup">
+                    <LoginForm onClose={() => setShowLoginForm(false)} />
+                </div>
             )}
         </>
-    )}
-</ul>
-</nav>
-</>
-);
+    );
 }
 
 export default Navbar;
